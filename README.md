3ad6ab512549098898454c5903c1567f78571531
00009967 <wiut00009967@gmail.com>
2021-04-07 18:50:28 +0500

    Uploading the ready version of web app inluding MD file-it is not ready yet

---
+++ README.md
@@ -0,0 +1,10 @@
+# H.R. Web Application
+
+### This web application allows users to create, edit, delete employees.
+
+To run this web application locally you must follow the steps indicated below:
+
+1. Clone this repository
+```bash
+git clone 
+```
--- app.js
+++ app.js
@@ -1,35 +1,92 @@
 const express = require('express');
 const app = express();
 
-const fs = require('fs');
+const fs = require('fs')
 
 app.set('view engine', 'pug')
 
 app.use('/static', express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 // Local host:8000
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
     res.render('home')
-});
+})
 
 // Login Page
 app.get('/login', (req, res) => {
     res.render('login')
-});
+})
 
 // Sign Up Page
 app.get('/signup', (req, res) => {
     res.render('signup')
-});
+})
 
 // All employee page
 app.get('/allemployees', (req, res) => {
-    res.render('allempl')
-});
+    
+    
+    fs.readFile('./data/hrDB.json', (err, data) => {
+        if (err) throw err
+
+        const people = JSON.parse(data)
+
+        res.render('allempl', {people: people})
+    })
+})
+
+app.get('/allemployees/:id', (req, res) => {
+    const id = req.params.id
+
+    fs.readFile('./data/hrDB.json', (err, data) => {
+        if (err) throw err
+
+        const people = JSON.parse(data)
+
+        const person = people.filter(person => person.id == id)[0]
+
+        res.render('detail', {person: person})
+
+
+    })
+})
 
 // New employee page
 app.get('/newemployee', (req, res) => {
     res.render('newempl')
+});
+
+app.post('/newemployee', (req, res) => {
+    const name = req.body.name
+    const surname = req.body.surname
+    const dob = req.body.dob
+    const education = req.body.education
+    const position = req.body.position
+
+    if(name.trim() === '' && surname.trim() === '' && dob.trim() === '' && education.trim() === '' && position.trim() === ''){
+        res.render('newempl', {error: true})
+    } else {
+        fs.readFile('./data/hrDB.json', (err, data) => {
+            if(err) throw err
+
+            const people = JSON.parse(data)
+
+            people.push({
+                id: id(),
+                name: name,
+                surname: surname,
+                dob: dob,
+                education: education,
+                position: position
+            })
+
+            fs.writeFile('./data/hrDB.json', JSON.stringify(people), err => {
+                if (err) throw err
+                res.render('newempl', { success: true })
+            })
+        })
+    }
 });
 
 app.listen(8000, err => {
@@ -37,3 +94,7 @@ app.listen(8000, err => {
 
     console.log('Server is running on port 8000!')
 })
+
+function id(){
+    return '_' + Math.random().toString(36).substr(2, 9);
+}
